87be520715f1315758a15300ff444b221d9385ba
Kenny Oh <koh@appnexus.com>
2016-04-19 00:13:09 -0400

    fixing indentation/spacing and removing unnecessary comments

--- classes/Course.java
+++ classes/Course.java
@@ -1,47 +1,42 @@
-/*
- * Work goes here :)
- * public Course {
- * int department;
- * int courseNumber;
- * int credits;
- *
- */
-
 public class Course {
-	private int department;
-	private int courseNum;
-	private String name;
-	private char day;
-	private int timeSlot; // this should be pulled from period object i think 
-	private int credits;
-	private int [] roster; //array language
-/* need 
- * constructor?
- */ 
-	public void Course (int department, int courseNum, String name, char day, int timeSlot, int credits) {
+  private int department;
+  private int courseNum;
+  private String name;
+  private char day;
+  private int timeSlot; // this should be pulled from period object i think
+  private int credits;
+  private int [] roster; //array language
+
+  public void Course(int department, int courseNum, String name, char day, int timeSlot, int credits) {
     this.department = department;
     this.courseNum = courseNum;
     this.name = name;
     this.day = day;
     this.timeSlot = timeSlot;
     this.credits = credits;
-	}
-  public int getDepartment(){
+  }
+
+  public int getDepartment() {
    return department;
   }
-  public int getCourseNum(){
+
+  public int getCourseNum() {
     return courseNum;
   }
-  public String getName(){
+
+  public String getName() {
     return name;
   }
-  public Period getPeriod(){
+
+  public Period getPeriod() {
     return timeSlot; //this is supposed to return the period
   }
-  public int getCredits(){
+
+  public int getCredits() {
     return credits;
   }
-  public Student[] getRoster(){
-    return roster
+
+  public Student[] getRoster() {
+    return roster;
   }
 }
