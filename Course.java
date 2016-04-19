e7ccd76706b3236df944302fb36c46d6ff8a52e7
Chris Presby <Cpresby1@mail.brookdalecc.edu>
2016-04-18 22:24:39 -0400

    added variables and began constructor

--- Course.java
+++ Course.java
@@ -1,8 +1,25 @@
-/**
+/*
  * Work goes here :)
- * public class Course {
+ * public Course {
  * int department;
  * int courseNumber;
  * int credits;
  * 
  */
+
+public class Course {
+	private int department;
+	private int courseNum;
+	private String name;
+	private char day;
+	private int timeSlot; // this should be pulled from period object i think 
+	private int credits;
+	private int []  Roster; 
+/* need to include an array of current roster
+ * constructor?
+ */
+	public Course (int department, int courseNum, String name, char day, int timeSlot, int credits){
+	
+	
+	}	
+}
