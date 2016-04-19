18157d965ea615da5c890a9959e7c4882ed4019b
Chris Presby <Cpresby1@mail.brookdalecc.edu>
2016-04-18 23:44:40 -0400

    almost finished course object

---
+++ classes/Course.java
@@ -0,0 +1,47 @@
+/*
+ * Work goes here :)
+ * public Course {
+ * int department;
+ * int courseNumber;
+ * int credits;
+ *
+ */
+
+public class Course {
+	private int department;
+	private int courseNum;
+	private String name;
+	private char day;
+	private int timeSlot; // this should be pulled from period object i think 
+	private int credits;
+	private int [] roster; //array language
+/* need 
+ * constructor?
+ */ 
+	public void Course (int department, int courseNum, String name, char day, int timeSlot, int credits) {
+    this.department = department;
+    this.courseNum = courseNum;
+    this.name = name;
+    this.day = day;
+    this.timeSlot = timeSlot;
+    this.credits = credits;
+	}
+  public int getDepartment(){
+   return department;
+  }
+  public int getCourseNum(){
+    return courseNum;
+  }
+  public String getName(){
+    return name;
+  }
+  public Period getPeriod(){
+    return timeSlot; //this is supposed to return the period
+  }
+  public int getCredits(){
+    return credits;
+  }
+  public Student[] getRoster(){
+    return roster
+  }
+}
